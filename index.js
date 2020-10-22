79b43e29346c847413859db59ab4d5569acfa271
Alberto De Agostini <albertodeagostini88@gmail.com>
2020-10-22 09:13:27 +0200

    Add basic mobile controls

--- index.js
+++ index.js
@@ -127,6 +127,55 @@ function handleInput(e) {
   }
 }
 
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];                                      
+  xDown = firstTouch.clientX;                                      
+  yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+  if ( ! xDown || ! yDown ) {
+    return;
+  }
+
+  var xUp = evt.touches[0].clientX;                                    
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+
+  if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+    if ( xDiff > 0 ) {
+      /* left swipe */ 
+      addNewDirection("left");
+      handle
+    } else {
+      /* right swipe */
+      addNewDirection("right");
+    }                       
+  } else {
+    if ( yDiff > 0 ) {
+      /* up swipe */ 
+      addNewDirection("up");
+    } else { 
+      /* down swipe */
+      addNewDirection("down");
+    }                                                                 
+  }
+  /* reset values */
+  xDown = null;
+  yDown = null;                                             
+};
+
+
 function addNewDirection(direction) {
   const buffer = gameState.directionBuffer;
   if (buffer[buffer.length - 1] !== direction && buffer.length < MAX_DIRECTION_BUFFER_LENGTH) {
@@ -268,6 +317,8 @@ function init() {
 
 startBtn.addEventListener("click", startGame);
 document.addEventListener("keydown", handleInput);
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
 window.addEventListener("resize", setBoardDimension);
 newGameBtn.addEventListener("click", init);
 
