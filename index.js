38a9fc288b1eb51962f6a8f4fa04e3925e9b731d
vaibhav <chandravaibhav65@gmail.com>
2020-10-28 00:14:01 +0530

    Scoring corrected

--- index.js
+++ index.js
@@ -128,11 +128,7 @@ function eatFood() {
     updateFoodCell("remove", gameState.food);
     createFood();
     updateFoodCell("add", gameState.food);
-    if(gameState.score/5 > 2){
-      gameState.score += Math.floor(gameState.score/10);
-    }else{
-      gameState.score++;
-    }
+    gameState.score += Math.ceil((gameState.score+1)/10);
     increaseSpeed();
     showScore();
     return true;
